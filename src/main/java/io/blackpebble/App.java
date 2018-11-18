2162327319690a4a0f6a28a9df193dfd5a39416f
ShamsUlAzeem <shamsazeem20@gmail.com>
2018-11-19 02:14:14 +0500

    Tested pdf generator

--- src/main/java/io/blackpebble/App.java
+++ src/main/java/io/blackpebble/App.java
@@ -1,13 +1,33 @@
 package io.blackpebble;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, DocumentException {
+        generatePDFFromHTML("C:\\Users\\shams\\Desktop\\invoice.html");
+    }
+
+    private static void generatePDFFromHTML(String filename) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document,
+                new FileOutputStream("src/output/html.pdf"));
+        document.open();
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream(filename));
+        document.close();
     }
 }
+
