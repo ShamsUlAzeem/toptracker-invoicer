2ecfcdf08e209ada3e787bba54653a5c2664ac28
shams <shamsazeem20@gmail.com>
2018-11-19 18:35:09 +0500

    Working project with working CSS

--- src/main/java/io/blackpebble/App.java
+++ src/main/java/io/blackpebble/App.java
@@ -1,12 +1,8 @@
 package io.blackpebble;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.html2pdf.HtmlConverter;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -16,18 +12,11 @@ import java.io.IOException;
  */
 public class App
 {
-    public static void main( String[] args ) throws IOException, DocumentException {
-        generatePDFFromHTML("C:\\Users\\shams\\Desktop\\invoice.html");
-    }
+    public static void main( String[] args ) throws IOException {
+        String filePath = "src/main/resources/sample-input.html";
+        String outFile = "src/main/resources/sample-output.pdf";
 
-    private static void generatePDFFromHTML(String filename) throws IOException, DocumentException {
-        Document document = new Document();
-        PdfWriter writer = PdfWriter.getInstance(document,
-                new FileOutputStream("src/output/html.pdf"));
-        document.open();
-        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
-                new FileInputStream(filename));
-        document.close();
+        HtmlConverter.convertToPdf(new FileInputStream(filePath), new FileOutputStream(outFile));
     }
 }
 
